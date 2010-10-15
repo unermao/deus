8f9da588aed2c166df91b707117de1ca76067590
picone.m <picone.m@a5033837-954d-0410-a456-d99eba8165e0>
2010-10-15 13:23:14 +0000

    - CityPath Updated - mobility model updated

--- deus/examples/D2V/src/it/unipr/ce/dsg/deus/example/d2v/mobilitymodel/CityPath.java
+++ deus/examples/D2V/src/it/unipr/ce/dsg/deus/example/d2v/mobilitymodel/CityPath.java
@@ -1,22 +1,71 @@
 package it.unipr.ce.dsg.deus.example.d2v.mobilitymodel;
 
+import it.unipr.ce.dsg.deus.core.Engine;
+
 import java.util.ArrayList;
 
 public class CityPath {
 
 	private ArrayList<CityPathPoint> pathPoints = null;
 	private boolean hasTrafficJam = false;
+	private double lenght = 0.0;
+	private int numOfCars = 0;
+	private double speedLimit = 0.0;
 	
 	/**
 	 * 
 	 */
 	public CityPath() {
 		this.pathPoints = new ArrayList<CityPathPoint>();
+		this.lenght = 0.0;
+		this.numOfCars = 0;
+		
+		//Random select a speed limit
+		int randomCase =  0;
+		
+		switch (randomCase) {
+		case 0:
+			this.speedLimit = 50.0;
+			break;
+		case 1:
+			this.speedLimit = 60.0;
+			break;
+		case 2:
+			this.speedLimit = 70.0;
+			break;
+		case 3:
+			this.speedLimit = 80.0;
+			break;
+
+			
+		default:
+			this.speedLimit = 50.0;
+			break;
+		}
+		
 	}
 	
 	public void addCityPathPoint(CityPathPoint point)
 	{
 		this.pathPoints.add(point);
+	}
+	
+	public void incrementNumOfCars()
+	{
+		this.numOfCars++;
+	}
+	
+	public void decrementNumOfCars()
+	{
+		if(this.numOfCars > 0)
+			this.numOfCars--;
+		else
+			System.err.println(this.getClass().getName()+" --> decrementNumOfCars: ERROR NUM OF CARS = 0 !!!");
+	}
+	
+	public void incrementPathLength(double len)
+	{
+		this.lenght += len;
 	}
 	
 	public GeoLocation getStartPoint()
@@ -49,6 +98,30 @@ public class CityPath {
 
 	public void setHasTrafficJam(boolean hasTrafficJam) {
 		this.hasTrafficJam = hasTrafficJam;
+	}
+
+	public double getLenght() {
+		return lenght;
+	}
+
+	public void setLenght(double lenght) {
+		this.lenght = lenght;
+	}
+
+	public int getNumOfCars() {
+		return numOfCars;
+	}
+
+	public void setNumOfCars(int numOfCars) {
+		this.numOfCars = numOfCars;
+	}
+
+	public double getSpeedLimit() {
+		return speedLimit;
+	}
+
+	public void setSpeedLimit(double speedLimit) {
+		this.speedLimit = speedLimit;
 	}
 	
 }
