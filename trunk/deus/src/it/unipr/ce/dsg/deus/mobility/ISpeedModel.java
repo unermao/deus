8345a193d6b3f8bf5f04f229854e4c04c93b9563
michele.amoretti <michele.amoretti@a5033837-954d-0410-a456-d99eba8165e0>
2010-12-14 12:29:05 +0000

    mobility package

---
+++ trunk/deus/src/it/unipr/ce/dsg/deus/mobility/ISpeedModel.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package it.unipr.ce.dsg.deus.mobility;
+
+/**
+ * @author Marco Picone picone@ce.unipr.it
+ *
+ */
+public interface ISpeedModel {
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double evaluateSpeedValue();
+	
+}
