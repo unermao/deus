ad0a3ab668cc887547bbbb2275e296baeea9cbdf
michele.amoretti <michele.amoretti@a5033837-954d-0410-a456-d99eba8165e0>
2013-01-28 21:16:39 +0000

    new queue policy!

---
+++ trunk/deus/src/it/unipr/ce/dsg/deus/core/RandomComparator.java
@@ -0,0 +1,37 @@
+package it.unipr.ce.dsg.deus.core;
+
+import java.util.Comparator;
+import java.util.Random;
+
+public class RandomComparator implements Comparator<Event> {
+
+	Random rng = null;
+	
+	public RandomComparator(Random rng) {
+		this.rng = rng;
+	}
+	
+	@Override
+	public int compare(Event o1, Event o2) {
+		System.out.println(o1.id + " compared to event " + o2.id);
+        int result = 0;
+        if (o1.triggeringTime < o2.triggeringTime)
+                result = -1;
+        else if (o1.triggeringTime == o2.triggeringTime) {
+                result = this.getNextComparatorInt();
+                System.out.println("result = " + result);
+        }
+        else if (o1.triggeringTime > o2.triggeringTime)
+                result = 1;
+        return result;
+	}
+
+	public int getNextComparatorInt() {
+		int i = rng.nextInt(2);
+		if (i == 0)
+			i = -1;
+		return i;
+	}
+
+	
+}
