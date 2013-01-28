c92959c0bc9e67fc6e95f53fd963b1787771e574
michele.amoretti <michele.amoretti@a5033837-954d-0410-a456-d99eba8165e0>
2013-01-28 22:03:02 +0000

    new queue policy!

--- trunk/deus/src/it/unipr/ce/dsg/deus/core/RandomComparator.java
+++ trunk/deus/src/it/unipr/ce/dsg/deus/core/RandomComparator.java
@@ -1,5 +1,15 @@
 package it.unipr.ce.dsg.deus.core;
 
+/**
+ * <p>
+ * The RandomComparator class is used to insert Event objects 
+ * in the event queue of the Engine, according to their timestamps.
+ * If two events have the same timestamp, their order is randomly chosen.
+ * </p>
+ * 
+ * @author Michele Amoretti (michele.amoretti@unipr.it)
+ * 
+ */
 import java.util.Comparator;
 import java.util.Random;
 
@@ -13,13 +23,13 @@ public class RandomComparator implements Comparator<Event> {
 	
 	@Override
 	public int compare(Event o1, Event o2) {
-		System.out.println(o1.id + " compared to event " + o2.id);
+		//System.out.println(o1.id + " compared to event " + o2.id);
         int result = 0;
         if (o1.triggeringTime < o2.triggeringTime)
                 result = -1;
         else if (o1.triggeringTime == o2.triggeringTime) {
                 result = this.getNextComparatorInt();
-                System.out.println("result = " + result);
+                //System.out.println("result = " + result);
         }
         else if (o1.triggeringTime > o2.triggeringTime)
                 result = 1;
